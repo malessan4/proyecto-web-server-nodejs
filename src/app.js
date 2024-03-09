cbadbf4716aa2ed3395110994ca877f7335a8e07
Matias-Alessandrello <matialess@gmail.com>
2024-03-09 02:41:34 -0300

    armando un web server

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
+const { envs } = require('./config/env')
+const { startServer } = require('./server/server')
+
+const main = () => {
+    startServer({
+        port: envs.PORT,
+        public_path: envs.PUBLIC_PATH
+    })
+}
+
 //Funcion agnóstica autoconvocada
 //Agnóstica porque no tiene nombre
 //Autoconvocada porque la ejecutamos con los parentesis
-
-const main = () => {
-    console.log('Aqui comenzaremos el servidor')
-}
-
-
 ( async () =>{
     main()
 })()
